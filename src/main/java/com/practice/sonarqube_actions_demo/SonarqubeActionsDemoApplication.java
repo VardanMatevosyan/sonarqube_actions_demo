6793208e8f0c5c995c52114203196eaf0685b1e7
VardanMatevosyan <vardanmt@gmail.com>
2025-11-27 23:26:02 +0400

    Init commit

---
+++ src/main/java/com/practice/sonarqube_actions_demo/SonarqubeActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.sonarqube_actions_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeActionsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarqubeActionsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/sonarqube_actions_demo/SonarqubeActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.sonarqube_actions_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeActionsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
